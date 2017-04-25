8bbada0375623b33f3198f33f3c36f40c182bbb6
dmatt <dmatt@disqus.com>
2017-04-24 22:33:27 -0700

    Add Wikipedia ajax requests on key presses

--- frontend/wikipedia-viewer/public/client.js
+++ frontend/wikipedia-viewer/public/client.js
@@ -6,7 +6,38 @@
 
 $(function() {
   console.log('hello world :o');
-  
+  $('input').keyup(function(event) {
+    let query = $('input').val();
+    $.ajax({
+      url: 'https://en.wikipedia.org/w/api.php',
+      type: 'GET',
+      dataType: 'json',
+      data: {
+        format: 'json',
+        origin: '*',
+        // TODO: Understand these at https://en.wikipedia.org/w/api.php or somewhere
+        action: 'query', //
+        generator: 'search', //
+        prop: 'extracts|info', //
+        inprop: 'url', //
+        exsentences: '1', //
+        exlimit: 'max', //
+        gsrsearch: query //
+      }
+    })
+    .done(function( data ) {
+      console.log("success");
+      console.log(data);
+      // TODO: turn all the object info into html elements, ex. data.query.pages[455682].canonicalurl
+      $('<li></li>').text(data.query.pages).appendTo('ul#searches');
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+    });
+  });
   $.get('/searches', function(searches) {
     searches.forEach(function(search) {
       $('<li></li>').text(search).appendTo('ul#searches');
