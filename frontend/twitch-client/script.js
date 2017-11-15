bc17358287afc537b8fa6fc2f6879a06764ed735
dmatt <dmatt@disqus.com>
2017-11-14 17:48:49 -0800

    Add twitch streams API call with client id

--- frontend/twitch-client/script.js
+++ frontend/twitch-client/script.js
@@ -1,8 +1,26 @@
 let topTenChannels = [];
 let statsObject = {};
+var streams = 'https://api.twitch.tv/helix/streams'
+var myHeaders = new Headers({
+  "Client-ID": "lnr7ybs8r23c75ogxck7pi5br73rje"
+});
+var myInit = { method: 'GET',
+headers: myHeaders,
+mode: 'cors',
+cache: 'default' };
 
-// get the top 10 users
+// function to fetch API and handles variables
+// function to get the top 10 channels
+// function to get the data for channel array
+// 
 
-fetch();
+var topTenChannelsElement = document.querySelector('#twitch-top-10');
+
+fetch(streams, myInit).then(function(response) {
+  return response.blob();
+}).then(function(myBlob) {
+  topTenChannels = myBlob // URL.createObjectURL(myBlob);
+  // myImage.src = objectURL;
+});
 
 console.log('hi this is some  fun twitchy stuff');
