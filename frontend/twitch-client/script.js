e03e2af93607c41ea21a3ed7bdf0872dc4ea0fc9
dmatt <dmatt@disqus.com>
2017-11-26 17:51:51 -0800

    Add error handling to fetch promise

--- frontend/twitch-client/script.js
+++ frontend/twitch-client/script.js
@@ -16,20 +16,35 @@ var twitchInit = {
 // function to get the top 10 channels
 // function to get the data for channel array
 
+// Store this "top 10" element as a variable to intert data into
 var topTenChannelsElement = document.querySelector('#twitch-top-10');
 
-// This needs to return a JSON object, currently something else
-function fetchAPI(api, endpoint, params, init) {
-  fetch(api+endpoint+params, init).then(function(response) {
-    return response.json();
-  }).then(function (response) {
-    response.data.forEach(element => {
-      topTenChannelsElement.innerHTML += element.display_name
-    });
-  })
+// Generic error handling for fetch promise
+function handleErrors(response) {
+  if (!response.ok) {
+      throw Error(response);
+  }
+  return response;
 }
 
-res = fetchAPI(twitchApi, 'users', '?id=1&44322889', twitchInit, topTenUsers);
+// Fetch function that handles HTTP status and network errors
+function fetchAPI(init, api, endpoint, params) {
+  fetch(api+endpoint+params, init)
+    .then(handleErrors)
+    .then(function (response) {
+      return response.json();
+    }).then(function (response) {
+      response.data.forEach(element => {
+        topTenChannelsElement.innerHTML += '<p>'+ JSON.stringify(element) + '</p>'
+      });
+    })
+    .catch(function(error) {
+      alert(error);
+    });
+  }
+
+fetchAPI(twitchInit, twitchApi, 'users', '?login=deinem&id=1');
+fetchAPI(twitchInit, twitchApi, 'streams', '?first=20');
 
 // in a loop output 1 json item to each div
 // in a loop or not give a class to each element filled with text (name, followers, )
