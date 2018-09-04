7cc6d502d1ea1cc0e34c1ea722fcceece56d8772
Daniel Matteson <dmatt@disqus.com>
2018-09-03 17:36:05 -0700

    Add functionality for watch button

--- frontend/twitch-client/script.js
+++ frontend/twitch-client/script.js
@@ -1,5 +1,5 @@
-let topTenUsers;
-var twitchApi = 'https://api.twitch.tv/helix/'
+let streams;
+var twitchApi = 'https://api.twitch.tv/kraken/'
 var myHeaders = new Headers({
   "Client-ID": "lnr7ybs8r23c75ogxck7pi5br73rje"
 });
@@ -28,7 +28,9 @@ function fetchAPI(init, api, endpoint, params) {
   fetch(api+endpoint+params, init)
     .then(handleErrors)
     .then(response => response.json())
-    .then(response => HTMLize(response))
+    .then(response => 
+      HTMLize(response)
+    )
     .catch(error => alert(error));
   }
 
@@ -40,42 +42,57 @@ fetchAPI(twitchInit, twitchApi, 'streams', '?first=10');
 // Loop through data and output on page with corresponding class names
 let targetElement = document.querySelector('#place-0');
 
-
 function HTMLize(response) {
-  response.data.forEach( (obj, i) =>
+  streams = response.streams
+  console.log("streams= ", streams)
+    response.streams.forEach( (obj, i) =>
     Object.keys(obj).forEach( function (attributeName) {
       switch (attributeName) {
-        case 'thumbnail_url':
+        case 'preview':
           let thumbnailElement = document.createElement("img");
           thumbnailElement.classList = "card-text card-img-top";
-          let width = /{width}/gi, height = /{height}/gi;
-          let newthumbnailUrl = obj[attributeName].replace(width, '640');
-          thumbnailElement.src = newthumbnailUrl.replace(height, '360');
+          let newthumbnailUrl = obj.preview.large;
+          thumbnailElement.src = newthumbnailUrl;
           document.querySelector('#place-'+i).parentNode.prepend(thumbnailElement);
         break;
-        case 'user_id':
+        case 'channel':
           let titleElement = document.createElement("h5");
           titleElement.classList = "card-title";
-          titleElement.innerHTML = obj[attributeName];
+          titleElement.innerHTML = obj.channel.display_name;
           document.querySelector('#place-'+i).appendChild(titleElement);
-        break;
-        case 'title':
           let textElement = document.createElement("p");
           textElement.classList = "card-text";
-          textElement.innerHTML = obj[attributeName];
+          textElement.innerHTML = obj.channel.status;
           document.querySelector('#place-'+i).appendChild(textElement);
         break;
       }
-      document.querySelector('#expand-'+i).addEventListener("click", handleClick, false);
     }
     )
   );
+  return response
 }
 
-function handleClick(event) {
+document.querySelectorAll('.expand, .watch').forEach(function() {addEventListener("click", handleClick, false)});
+
+function handleClick(e) {
   // open / close the clicked parent div with every click
-  console.log(event)
-  event.target.parentElement.querySelector('.card-text').classList.toggle('collapsed');
-  let collapseButtonState = event.srcElement
-  collapseButtonState.innerHTML == '🐵' ? collapseButtonState.innerHTML = '🙈' : collapseButtonState.innerHTML = '🐵';
-}
+  if (e.target.classList.contains("expand")) {
+    event.target.parentElement.querySelector('.card-text').classList.toggle('collapsed');
+    let collapseButtonState = event.srcElement
+    collapseButtonState.innerHTML == '🐵' ? collapseButtonState.innerHTML = '🙈' : collapseButtonState.innerHTML = '🐵';
+  }
+  // start a new twitch viewer with the desired channel
+  else if (e.target.classList.contains("watch")) {
+    let nextChannel = streams[e.target.id.split("-")[1]].channel.name
+    document.querySelector("#twitch-embed").innerHTML = null;
+    new Twitch.Embed("twitch-embed", {
+      width: 600,
+      channel: nextChannel,
+    });
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+    });
+  }
+} 
